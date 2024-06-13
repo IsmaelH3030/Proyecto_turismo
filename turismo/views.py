2b126c6799d1532f0983a6afa5179621cdc5f1f2
Ismael Horta <is.horta@duocuc.cl>
2024-06-13 16:55:07 -0400

    con login

--- turismo/views.py
+++ turismo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def inicio(request):
-    return render(request, 'inicio.html')
+def Login(request):
+    return render(request, 'Login.html')
