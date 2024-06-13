64049cabb0ea57a5ece4c6c35fb52aebbe6acb82
Ismael Horta <is.horta@duocuc.cl>
2024-06-13 19:39:34 -0400

    terminado sin paisajes

--- turismo/models.py
+++ turismo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Servicios(models.Model):
+    id = models.AutoField(primary_key=True)  # Django crea automáticamente un campo id, así que esto es opcional.
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    foto = models.ImageField(upload_to='fotos_servicios/')
+    descripción_del_lugar = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Servicio"         #Nombre singular del modelo
+        verbose_name_plural = "Servicios" #Nombre plural del modelo
+        ordering = ['precio']             #Orden por defecto al consultar la base de datos
+                                          #verbose name = nombre detallado
+
+    def __str__(self):
+        return f"{self.descripción_del_lugar} - ${self.precio}"
--- turismo/views.py
+++ turismo/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def Login(request):
     return render(request, 'Login.html')
 
 def index(request):
-    return render(request, 'index.html')
+    servicios = Servicios.objects.all()
+    context = {"servicios":servicios}
+    return render(request, 'index.html',context)
+
+
+
+# def index(request):
+#     alumnos = Alumno.objects.all()
+#     context = {"alumnos":alumnos}
+#     return render(request, 'index.html', context)
